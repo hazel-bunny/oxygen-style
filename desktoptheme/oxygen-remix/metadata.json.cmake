ce44b4b840fc735fb5ea8500c57ae7abee8dec5d
Hazel Bunny <hazel_bunny@disroot.org>
2026-03-06 23:18:17 +0600

    Add new plasma theme

---
+++ desktoptheme/oxygen-remix/metadata.json.cmake
@@ -0,0 +1,17 @@
+{
+    "KPlugin": {
+        "Authors": [
+            {
+                "Email": "hazel_bunny@disroot.org",
+                "Name": "Hazel Bunny"
+            }
+        ],
+        "Description": "Oxygen plasma theme, remixed for KDE Plasma 6",
+        "EnabledByDefault": true,
+        "Id": "oxygen-remix",
+        "License": "GPL",
+        "Name": "Oxygen Remix",
+        "Version": "6.6.2"
+    },
+    "X-Plasma-API": "6.0"
+}
